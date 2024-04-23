51dd88947843e6ea2919584e8e8a92114d57785a
Shanjiv A <shanjiv177@gmail.com>
2024-04-23 20:56:55 +0530

    NavBar redesign with shadcn

---
+++ src/pages/signin/signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <div className='flex flex-col items-center p-10 justify-center'>
+        <div className="title text-4xl">
+            Sign In
+        </div>
+        
+    </div>
+  )
+}
