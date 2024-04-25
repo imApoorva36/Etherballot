0327721d8964a7ed9e39cc2de8f45fa34e04f8e3
Shanjiv A <shanjiv177@gmail.com>
2024-04-25 13:01:38 +0530

    Create Elections page

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,140 +1,109 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
 import React from 'react'
-import { useForm } from 'react-hook-form'
-import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import * as z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { DateTimePicker } from '@/components/ui/date-time-picker/date-time-picker';
-
-const dateTimeObjectSchema = z.object({
-  calendar: z.object({
-    identifier: z.string(),
-  }),
-  era: z.string(),
-  year: z.number(),
-  month: z.number(),
-  day: z.number(),
-  hour: z.number(),
-  minute: z.number(),
-  second: z.number(),
-  millisecond: z.number(),
-});
-
-const formSchema = z.object({
-  electionTitle: z.string(),
-  candidates: z.string(),
-  startDate: dateTimeObjectSchema,
-  endDate: dateTimeObjectSchema,
-})
+import { useState } from 'react'
+import { DateTimePicker } from '@/components/ui/date-time-picker/date-time-picker'
 
 export default function Admin() {
 
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      electionTitle: '',
-      candidates: '',
-    }
-  });
+  const [electionTitle, setElectionTitle] = useState('')
+  const [startTime, setStartTime] = useState({})
+  const [endTime, setEndTime] = useState({})
+  const [candidates, setCandidates] = useState([])
 
-  const submitHandler = (values) => {
-    console.log(values);
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    const formObj = {
+      electionTitle: electionTitle,
+      startTime: startTime,
+      endTime: endTime,
+      candidates: candidates,
+    }
+
+    console.log(formObj)
+  }
+
+  const KeyHandler = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (e.target.value != "") {
+        const newCandidates = [...candidates,e.target.value]
+        setCandidates(newCandidates)
+        e.target.value = ""
+      }
+    }
   }
 
   return (
-    <div className='flex flex-col items-center p-10 gap-2 w-full'>
-        <div className="title text-4xl">
-            Create an election
+    <div className="admin flex flex-col justify-center items-center m-10 w-full">
+      <div className="title text-4xl">
+        Create an election
+      </div>
+      <div className={
+        (candidates.length > 0) ? 'w-full flex justify-between p-10 gap-4' :
+        'w-full flex justify-center'
+      }>
+        <div className="form w-1/2 items-center justify-center">
+          <form onSubmit={SubmitHandler} className='flex flex-col gap-4'>
+            <div className="electionTitle flex flex-col gap-2">
+            <Label className="text-2xl">Election Title</Label>
+            <Input type="text" id="electionTitle" value={electionTitle} placeholder="Enter election title" onChange={(e) => {setElectionTitle(e.target.value)}} />
+            
+            </div>
+            <div className="electionCandidates">
+              <Label className="text-2xl">Candidates</Label>
+              <Input type="text" id="candidates" onKeyDown={KeyHandler} placeholder={
+                (candidates.length > 0 ) ? "Enter another candidate name and press enter" :
+                "Enter a candidate name and press enter"
+              } />
+            </div>
+            <div className="electionStartTime">
+              <Label className="text-2xl">Start Date</Label>
+              <DateTimePicker id="startTime" onChange={
+                (obj) => {
+                  setStartTime(obj)
+                }
+              } granularity={"minute"}/>
+            </div>
+            <div className="electionEndTime">
+              <Label className="text-2xl">End Time</Label>
+              <DateTimePicker id="endTime" onChange={
+                (obj) => {
+                  setEndTime(obj)
+                }
+              } granularity={"minute"}/>
+            </div>
+            <Button type="submit">Create an election</Button>
+          </form>
         </div>
-        <div className='flex flex-col items-center justify-between gap-8 w-3/4 m-8 p-4'>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(submitHandler)} className='space-y-8 w-1/2'>
-              <FormField 
-                control={form.control}
-                name="electionTitle"
-                render = {
-                  ({field}) => {
-                    return(
-                      <FormItem>
-                        <FormLabel className="text-2xl">Election title</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Enter election title" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )
-                  }
-                }
-              />
-              <FormField 
-                control={form.control}
-                name="candidates"
-                render = {
-                  ({field}) => {
-                    return(
-                      <FormItem>
-                        <FormLabel className="text-2xl">Candidates</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Enter candidates seperated by commas" {...field} />
-                        </FormControl>
-                      </FormItem>
-                    )
-                  }
-                }
-              />
-              <FormField 
-                control={form.control}
-                name="startDate"
-                render = {
-                  ({field}) => {
-                    return(
-                      <FormItem>
-                        <FormLabel className="text-2xl">Start Date and Time</FormLabel>
-                        <FormControl>
-                          <DateTimePicker 
-                          value={field.value}
-                          onChange={date => field.onChange(date)}
-                          granularity={"minute"} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )
-                  }
-                }
-              />
-              <FormField 
-                control={form.control}
-                name="endDate"
-                render = {
-                  ({field}) => {
-                    return(
-                      <FormItem>
-                        <FormLabel className="text-2xl">End Date and Time</FormLabel>
-                        <FormControl>
-                          <DateTimePicker 
-                          value={field.value}
-                          onChange={date => field.onChange(date)}
-                          granularity={"minute"} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )
-                  }
-                }
-              />
-              <Button type="submit" className="w-1/2">Create Election</Button>
-            </form>
-          </Form>
-        </div>
+        {
+          (candidates.length > 0) ?
+          <div className="candidates w-1/2">
+            <div className="title text-2xl text-center">
+              Selected Candidates
+            </div>
+            <div className='p-10'>
+              {
+                candidates.map((candidate, index) => (
+                  <ul>
+                    <div className='flex items-center justify-between'>
+                      <li key={index} className='text-xl p-5'>{index +1}. {candidate}</li>
+                      <Button onClick={() => {
+                          const newCandidates = [...candidates]
+                          newCandidates.splice(index,1)
+                          setCandidates(newCandidates)
+                        }
+                      }>Remove Candidate</Button>
+                    </div>
+                  </ul>
+                ))
+              }
+            </div>
+          </div>
+          : null
+        }
+      </div>
     </div>
   )
 }
