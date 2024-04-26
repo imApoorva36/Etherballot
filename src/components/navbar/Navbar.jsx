a42e262e5929df242d1851b4363d3e98d57ea838
Shanjiv A <shanjiv177@gmail.com>
2024-04-26 22:28:36 +0530

    Added user greeting

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { Button } from '../ui/button'
 import { useState } from 'react'
 import logo from '../../assets/logo.svg'
+import userContext from '@/scripts/userContext'
 
 export default function Navbar() {
 
-    const [isConnected, setIsConnected] = useState();
-    const [isSignedIn, setIsSignedIn] = useState();
+    const userState = useContext(userContext)
+
 
     const connectMetaMask = async () => {
         let provider;
@@ -14,7 +15,7 @@ export default function Navbar() {
           provider = window.ethereum;
           try {
             await provider.request({method: 'eth_requestAccounts'});
-            setIsConnected(true);
+            userState.setIsConnected(true);
           }
           catch (e) {
             console.log(e);
@@ -22,9 +23,33 @@ export default function Navbar() {
         }
       }
 
+    const SignIn = async () => {
+      let provider;
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          const accounts = await provider.request({method: 'eth_requestAccounts'})
+          const account = accounts[0]
+          const newUser = {userAddress: account}
+          userState.setUser(newUser)
+          userState.setIsSignedIn(true)
+        }
+        catch (e) {
+
+        }
+      }
+    }
+
+    const SignOut = () => {
+      userState.setUser(null)
+      userState.setIsSignedIn(false)
+    }
+
+
     useEffect(() => {
         connectMetaMask();
-    },[]);
+        userState.setUser(userState.user)
+    },[userState.user]);
 
   return (
     <div className="flex border-b px-10 py-5 justify-between items-center">
@@ -38,7 +63,7 @@ export default function Navbar() {
         </div>
         <div className='flex justify-between gap-8'>
             {
-                isConnected ?
+                userState.isConnected ?
                 <>
                     <Button variant="ghost">Wallet Connected</Button>
                 </>
@@ -48,13 +73,15 @@ export default function Navbar() {
                 </>
             }
             {
-                isSignedIn ?
-                <>
+                userState.isSignedIn ?
+                <div className='flex items-center gap-2'>
+                    <div>Hello {userState.user.userAddress}</div>
                     <Button variant="ghost">Dashboard</Button>
-                </>
+                    <Button onClick={SignOut}>Sign Out</Button>
+                </div>
                 :
                 <>
-                    <Button>Sign In / Register</Button>
+                    <Button onClick={SignIn}>Sign In / Register</Button>
                 </>
             }
         </div>
---
+++ src/pages/elections/Elections.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Elections() {
+  return (
+    <div className="flex flex-col items-center m-10 gap-4">
+        <div className="text-4xl">Elections</div>
+        <div className='border'>
+            Cards to be present here
+        </div>
+    </div>
+  )
+}
