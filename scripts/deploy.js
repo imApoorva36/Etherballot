637ad52ad875569bcfa1c28889edf01f869f0966
ahan-halder <162003553+ahan-halder@users.noreply.github.com>
2024-05-01 09:29:05 +0530

    Smart Contract, Hardhat Config, Depoly code (#18)
    * Contract,Hardhat
    
    * mend
    
    * Smart Contract, Deploy code, Hardhat Config
    
    * Smart Contract Changes

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+import pkg from 'hardhat';
+const { ethers } = pkg;
+
+async function main() {
+    // Get the ContractFactory
+    const EtherBallot = await ethers.getContractFactory("etherballot");
+  
+    // Deploy the contract
+    const etherBallot = await EtherBallot.deploy();
+  
+    await etherBallot.waitForDeployment();
+  
+    // Retrieve and log the contract address
+    console.log("EtherBallot deployed to:", await etherBallot.getAddress());
+  }
+  
+  // Call main function
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+  
