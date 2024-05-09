5c0cbf0eed94252f872690bfd35659dae2212c8d
Shanjiv A <shanjiv177@gmail.com>
2024-05-09 19:17:33 +0530

    Landing page redesign-1

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -5,8 +5,8 @@ import { Button } from '@/components/ui/button'
 export default function LandingPage() {
   return (
     <div className='m-20 flex flex-col gap-2'>
-        <h1 className='text-8xl font-jarkata font-bolder'>Welcome to</h1>
-        <h1 className='text-9xl font-poppins font-bold'>EtherBallot</h1>
+        <h1 className='text-8xl font-jarkata'>Welcome to</h1>
+        <h1 className='text-9xl font-poppins font-bolder'>EtherBallot</h1>
         <div className='text-4xl text-gray-400 font-noto'>Every election is determined by the people who show up</div>
         <div className='text-4xl text-gray-400 font-noto'>Now that's a quote</div>
         <div className='text-4xl text-gray-400 font-noto'>Anyways, you showed up!</div>
